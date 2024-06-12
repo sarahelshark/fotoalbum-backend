83852805f27ab915ad57c0f734b7a65fd9ce54d6
sarahelshark <sarahelsh183@gmail.com>
2024-06-12 21:51:25 +0200

    [add] fixed heights for cards + cards with my Photo elements + card footer

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -24,21 +24,26 @@
   
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           @forelse ($photos as $photo)
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-  
+          <div class="col" >
+            <div class="card h-100 shadow-sm" id="{{$photo->id}}">
+              <img src="{{$photo->cover_image}}" class="card-img-top" width="100%" height="225" alt="{{$photo->name}}">  
               <div class="card-body">
+                <small class="text-muted" > {{$photo->id}}</small>
+                <h4>{{$photo->name}}</h4>
+                
                 <p class="card-text">{{$photo->description}}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
-                  </div>
-                  
-                </div>
+
               </div>
+              <div class="card-footer bg-white py-3"> <div class="d-flex flex-column align-items-center">
+                <div class="btn-group">
+           
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+                </div>
+                
+              </div>
+          </div>
             </div>
           </div>
           @empty
