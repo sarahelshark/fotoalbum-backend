97fe1874eb446b1bd01674c9516779ed3bdc3980
sarahelshark <sarahelsh183@gmail.com>
2024-06-13 20:21:07 +0200

    [add] style to view create + form errors  + partials>errors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,64 +21,86 @@
     </section>
 
     @auth
-    <div class="album py-5 bg-light">
-      <div class="container">
 
-        <h2 class="fw-light mb-3 text-primary ">Latest Pictures</h2>
+    <div class="container px-4 py-5" id="custom-cards">
+      
+      <h2 class="fw-light mb-3 text-primary pb-2 border-bottom border-primary">Latest Pictures</h2>
   
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-  
-              <div class="card-body">
-                <p class="card-text">This is a title.</p>
-                <div class="d-flex justify-content-between align-items-center">
+      <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-3">
+        <div class="col">
+          <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('unsplash-photo-1.jpg');">
+            <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+              <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h3>
+              <ul class="d-flex list-unstyled mt-auto">
+                <li class="me-auto">
                   <div class="btn-group">
                     <button type="button" class="btn btn-sm btn-outline-primary">View</button>
                     
                   </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
+                </li>
+                <li class="d-flex align-items-center me-3">
+                  <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>
+                  <small>Earth</small>
+                </li>
+                <li class="d-flex align-items-center">
+                  <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
+                  <small>3d</small>
+                </li>
+              </ul>
             </div>
           </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
   
-              <div class="card-body">
-                <p class="card-text">This is a title.</p>
-                <div class="d-flex justify-content-between align-items-center">
+        <div class="col">
+          <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('unsplash-photo-2.jpg');">
+            <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+              <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Much longer title that wraps to multiple lines</h3>
+              <ul class="d-flex list-unstyled mt-auto">
+                <li class="me-auto">
                   <div class="btn-group">
                     <button type="button" class="btn btn-sm btn-outline-primary">View</button>
                     
                   </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
+                </li>
+                <li class="d-flex align-items-center me-3">
+                  <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>
+                  <small>Pakistan</small>
+                </li>
+                <li class="d-flex align-items-center">
+                  <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
+                  <small>4d</small>
+                </li>
+              </ul>
             </div>
           </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
   
-              <div class="card-body">
-                <p class="card-text">This is a title.</p>
-                <div class="d-flex justify-content-between align-items-center">
+        <div class="col">
+          <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('unsplash-photo-3.jpg');">
+            <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+              <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Another longer title belongs here</h3>
+              <ul class="d-flex list-unstyled mt-auto">
+                <li class="me-auto">
                   <div class="btn-group">
                     <button type="button" class="btn btn-sm btn-outline-primary">View</button>
                     
                   </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
+                </li>
+                <li class="d-flex align-items-center me-3">
+                  <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>
+                  <small>California</small>
+                </li>
+                <li class="d-flex align-items-center">
+                  <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>
+                  <small>5d</small>
+                </li>
+              </ul>
             </div>
           </div>
-          
         </div>
       </div>
     </div>
+
     
     
     @endauth     
