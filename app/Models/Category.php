3950f2fde35eba3ad360b376e5050306688d15d5
sarahelshark <sarahelsh183@gmail.com>
2024-06-16 13:42:04 +0200

    [add]
    relationship in my models , add methods- $photo = Photo::find(1) -$photo->category_id=3;-$photo->save()

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
+    /**
+     * Get all of the Photos for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function Photos(): HasMany
+    {
+        return $this->hasMany(Photo::class);
+    }
 }
--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Photo extends Model
 {
     use HasFactory;
-
+    
     protected $fillable=['name','cover_image','description'];
+    /**
+     * Get the user that owns the Photo
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
