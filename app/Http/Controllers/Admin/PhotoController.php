9785ff7492953db14fa107bd51b9259f30842f75
sarahelshark <sarahelsh183@gmail.com>
2024-06-17 22:09:36 +0200

    [FIX] minor bug

--- app/Http/Controllers/Admin/PhotoController.php
+++ app/Http/Controllers/Admin/PhotoController.php
@@ -52,7 +52,7 @@ class PhotoController extends Controller
      */
     public function show(Photo $photo)
     {
-        return view('admin.photos.show', compact('photos'));
+        return view('admin.photos.show', compact('photo'));
     }
 
     /**
@@ -61,7 +61,7 @@ class PhotoController extends Controller
     public function edit(Photo $photo)
     {
         $categories = Category::all();  
-        return view('admin.photos.edit',compact('photos', 'categories'));
+        return view('admin.photos.edit',compact('photo', 'categories'));
     }
 
     /**
